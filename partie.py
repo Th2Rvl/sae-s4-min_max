f28fa3b6759d306b10022e310373cfb1fb132360
Th2Rvl <theophane.derouville@gmail.com>
2026-01-21 10:32:38 +0100

    Avancement de la partie

--- partie.py
+++ partie.py
@@ -2,14 +2,22 @@ import random
 
 from grille import Grille
 from joueur import Joueur, JoueurHumain, IA
-
+from vue import afficherBienvenue, demanderNomJoueur, afficherGrille, demanderCoup
 
 
 class Partie:
     def __init__(self):
-        self.joueurHumain = JoueurHumain("Joueur1", True if random.randint(0,1) == 0 else False)
+        self.joueurHumain = JoueurHumain(demanderNomJoueur(), True if random.randint(0,1) == 0 else False)
         self.ia = IA(False if self.joueurHumain.aSonTour else True)
         self.grille = Grille()
 
     def lancerPartie(self):
+        afficherBienvenue()
         while not self.grille.estComplete():
+            afficherGrille(self.grille)
+
+            coup = ""
+            if self.joueurHumain.aSonTour:
+                coup = demanderCoup(self.joueurHumain.nom)
+            else: coup = self.ia.choisirCoup()
+            self.grille.modifierGrille(coup[0], coup[1], self.joueurHumain.pion)
